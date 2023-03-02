fe05d27611c60729a57a155158b8def2d3e36854
JuanDavidFern <107356975+JuanDavidFern@users.noreply.github.com>
2023-03-02 12:15:57 +0100

    second commit

---
+++ src/models/Materia.java
@@ -0,0 +1,14 @@
+package models;
+
+public class Materia {
+	
+	private int id;
+	private String nombre;
+	private String acronimo;
+	private int curso_id;
+
+	public Materia() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
